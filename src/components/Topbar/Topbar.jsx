0bd138bce68dc4a8b102eca2706194304f1d940a
imlilac <lilac01vv@gmail.com>
2024-10-23 21:07:13 +0330

    topbar finish

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import "./Topbar.css";
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import LanguageIcon from '@mui/icons-material/Language';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export default function Topbar() {
    return ( 
       <div className="topbar">
          <div className="topbarWrapper">
             <div className="topLeft">
-               <span className="logo">Lilac 🪻</span>
+               <span className="logo">V.</span>
             </div>
 
             <div className="topRight">
                <div className="topbarIconContainer">
                   <NotificationsIcon/>
+                  <span className="topIconBadge">7</span>
                </div>
-
+               <div className="topbarIconContainer">
+                  <LanguageIcon/>
+                  <span className="topIconBadge">2</span>
+               </div>
+               <div className="topbarIconContainer">
+                  <SettingsIcon/>
+               </div>
+               <img src="images/logo1.jpg" className="topAvatar" alt="" />
             </div>
          </div>
       </div>
