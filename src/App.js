1a32835e15f6d489e7875e82144bb1ce455e2694
imlilac <lilac01vv@gmail.com>
2024-10-24 17:23:21 +0330

    add sidebar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import routes from "./routes";
 import { useRoutes } from "react-router-dom";
 import Topbar from "./components/Topbar/Topbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import "./App.css";
 
 export default function App() {
    let router = useRoutes(routes);
    return (
       <>
          <Topbar />
-         {router}
+         <div className="container">
+            <Sidebar />
+            {router}
+         </div>
       </>
    );
 }
