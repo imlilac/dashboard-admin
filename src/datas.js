45ea93c4607a5652cc45bd5482d9d709073d97e2
imlilac <lilac01vv@gmail.com>
2024-10-27 22:54:25 +0330

    add chart component

---
+++ src/datas.js
@@ -0,0 +1,52 @@
+let xAxisData = [
+   {
+      name: "Januery",
+      Sale: 222_000,
+   },
+   {
+      name: "February",
+      Sale: 32_000,
+   },
+   {
+      name: "March",
+      Sale: 102_000,
+   },
+   {
+      name: "April",
+      Sale: 152_000,
+   },
+   {
+      name: "May",
+      Sale: 252_000,
+   },
+   {
+      name: "June",
+      Sale: 82_000,
+   },
+   {
+      name: "July",
+      Sale: 42_000,
+   },
+   {
+      name: "Agoust",
+      Sale: 10_000,
+   },
+   {
+      name: "September",
+      Sale: 92_580,
+   },
+   {
+      name: "October",
+      Sale: 162_000,
+   },
+   {
+      name: "November",
+      Sale: 182_050,
+   },
+   {
+      name: "December",
+      Sale: 272_300,
+   },
+];
+
+export { xAxisData };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Feature from "../../components/Feature/Feature";
+import Chart from "../../components/Chart/Chart";
+import { xAxisData } from "../../datas";
+import "./Home.css";
 
-export default function home() {
-   return <Feature />;
+export default function Home() {
+   return (
+      <div className="home">
+         <Feature />
+         <Chart grid title="Monthly Sale" data={xAxisData} dataKey="Sale" />
+      </div>
+   );
 }
